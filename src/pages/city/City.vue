7390ea775246278c031b2e1b08a759ef8ca604e2
jacobhsu <jacob.hsu.tw@gmail.com>
2020-02-18 10:00:50 +0800

    page City Header/Search/List ui

---
+++ src/pages/city/City.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <city-header></city-header>
+    <city-search :cities="cities"></city-search>
+    <city-list
+      :cities="cities"
+      :hot="hotCities"
+      :letter="letter"
+    ></city-list>
+    <city-alphabet
+      :cities="cities"
+      @change="handleLetterChange"
+    ></city-alphabet>
+  </div>
+</template>
+
+<script>
+import CityHeader from './components/Header'
+import CitySearch from './components/Search'
+import CityList from './components/List'
+export default {
+    name: 'City',
+    components: {
+      CityHeader,
+      CitySearch,
+      CityList,
+  },
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
